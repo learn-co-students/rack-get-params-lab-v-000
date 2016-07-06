e5d95f91c675b6b491cbbe7e445e1dcc8d1f6168
johnsontaiwo <josseytee@gmail.com>
2016-07-06 19:55:21 -0400

    added /cart and /add paths

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,6 +15,24 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      added_item = req.params["item"]
+      if @@items.include?(added_item)
+        @@cart << added_item
+        resp.write "added #{added_item}"
+       binding.pry
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
