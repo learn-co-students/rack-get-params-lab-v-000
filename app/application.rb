8f6f51cdbff2862750c829050b4e3105a311527b
Emmanuel Owusu <e_owusu@live.com>
2016-10-25 12:33:03 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,5 @@
 class Application
 
-
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
 
@@ -8,46 +7,41 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-#cart code
-    if req.path.match(/cart/)
-        @@cart.each do |cart_item|
-          resp.write "#{cart_item}\n"
-        end
-    else
-        resp.write "Your cart is empty"
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
       end
-
-#add code
-    if req.path.match(/add/)
-      search_term = req.params["key"]
-
-        if @@items.include?(search_term)
-          @@cart << search_term
-          resp.write "added #{search_term}"
-        else
-          resp.write "We don't have that item"
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
         end
-
+      end
+    elsif req.path.match(/add/)
+      added_item = req.params["item"]
+      if @@items.include? added_item
+        @@cart << added_item
+        resp.write "added #{added_item}"
+      else
+        resp.write "We don't have that item!"
+      end
     else
-          resp.write "Your cart is empty"
-        end
+      resp.write "Path Not Found"
+    end
 
+    resp.finish
+  end
 
-#items code
-
-
-      resp.finish
-
-    end #call(env)
-end #class app
-
-
-  #if cart
-	#cart code
-#elsif add
-	#add code
-#elsif items
-	#items code
-#else
-	#whatever
-#send
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
