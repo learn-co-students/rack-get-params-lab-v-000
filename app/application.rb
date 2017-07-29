a715cc7c52aa2fa5530f5140245b03d2cbefbfd4
Juan Gongora <gongora.animations@gmail.com>
2017-07-29 22:30:15 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,14 @@ class Application
       else
         display_commerce(@@cart, resp)
       end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
