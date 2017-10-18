97c830e55dc114c94858e971499464740ee7d28d
Guesly  Germain <germain1211@outlook.com>
2017-10-18 16:42:15 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -8,19 +8,33 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    # binding.pry
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
-      resp.write handle_search(search_term)
+        resp.write handle_search(search_term)
     elsif  req.path.match(/cart/)
-      @@cart.collect do |cart_item|
-        resp.write "#{cart_item}\n"
-      end
+        if @@cart.empty?
+          resp.write "Your cart is empty"
+        else
+          @@cart.each do |cart_item|
+            resp.write "#{cart_item}\n"
+          end
+        end
+      elsif req.path.match(/add/)
+        item = req.params["item"]
+        if @@items.include?(item)
+          @@cart <<  item
+          resp.write "added #{item}"
+        else
+          # binding.pry
+          resp.write "We don't have that item"
+        end
     else
-      resp.write "Path Not Found"
+        resp.write "Path Not Found"
     end
 
     resp.finish
@@ -34,13 +48,4 @@ class Application
     end
   end
 
-  # def shopping_cart_rack_app
-  #   if req.path.match(/cart/)
-  #     @@cart.collect do |cart_item|
-  #       resp.write "#{cart_item}\n"
-  #     end
-  #     else
-  #       return @@cart
-  #     end
-  #   end
 end
