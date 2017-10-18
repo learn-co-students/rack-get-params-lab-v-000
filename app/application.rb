a06a5cc047d7a5972c67d32d603650083d280b74
Kyle Poretto <kyleporetto@gmail.com>
2017-10-18 14:55:37 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,10 +14,25 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty."
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      items_to_add = req.params["item"]# Create a new route called /add that takes in a GET param with the key item. This should check to see if that item is in @@items and then add it to the cart if it is. Otherwise give an error
+        if @@items.include?(items_to_add)
+          @@cart << items_to_add
+          resp.write "added #{items_to_add}"
+        else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
