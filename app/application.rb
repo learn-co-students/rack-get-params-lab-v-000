0a5a25879ae0ffeab768c917f880fa0dd7a363ef
Adam Tropp <adt6261@gmail.com>
2017-11-09 22:29:00 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,16 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart. each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include(search_term)
+        @@cart << search_term
+      else
+        resp.write "No item matching that name"
     else
       resp.write "Path Not Found"
     end
