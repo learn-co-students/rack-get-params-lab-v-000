c3e630cd9d979331d1106f30a9109d252465c18a
Hunter Kleindl <hkleindl@gmail.com>
2017-11-30 16:18:15 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,9 +11,28 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      if !@@cart.empty?
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include?(item_to_add)
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
