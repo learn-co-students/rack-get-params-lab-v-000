2f4c94b3865c2bf31f3ab22179e17bd5e0848c68
Hannah Carnes <hecarnes@gmail.com>
2018-01-03 15:48:15 -0500

    two tests passing

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,15 +11,31 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    end
+    elsif req.path.match(/add/)
+      new_item = req.params["item"]
+      resp.write handle_add_item(new_item)
+    elsif
       resp.write "Path Not Found"
     end
+  
+    # elsif req.path.match(/search/)
+    #   search_term = req.params["q"]
+    #   resp.write handle_search(search_term)
+    # else
+    #   resp.write "Path Not Found"
+    # end
 
     resp.finish
   end
+end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -27,4 +44,17 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-end
+
+  def handle_add_item(new_item)
+    if @@item.include?(new_item)
+      @@cart << new_item
+      return "added #{new_item}"
+    else
+      return "We don't have that item"
+    end
+  end
+
+
+
+#Create a new class array called @@cart to hold any items in your cart
+#Create a new route called /cart to show the items in your cart
