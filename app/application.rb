8a1814fd12b6d2dd08b8f58d915ac3c002313887
Andy Maskiell <maskiella@gmail.com>
2018-02-05 20:29:19 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -15,24 +15,10 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      # resp.write handle_cart
-      if @@cart.empty?
-        resp.write "Your cart is empty."
-      else
-        resp.write "Your cart includes the following items:\n"
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
-      end
+      resp.write handle_cart
     elsif req.path.match(/add/)
       item_to_add = req.params["item"]
-      # resp.write add_item_to_cart(item_to_add)
-      if @@items.include?(item_to_add)
-        @@cart << item_to_add
-        resp.write "We've added #{item_to_add} to your cart."
-      else
-        resp.write "We don't have that item."
-      end
+      resp.write add_item_to_cart(item_to_add)
     else
       resp.write "Path Not Found"
     end
@@ -48,23 +34,24 @@ class Application
     end
   end
 
-  # def handle_cart
-  #   if @@cart.empty?
-  #     resp.write "Your cart is empty."
-  #   else
-  #     resp.write "Your cart includes the following items:\n"
-  #     @@cart.each do |item|
-  #       resp.write "#{item}\n"
-  #     end
-  #   end
-  # end
+  def handle_cart
+    if @@cart.empty?
+      "Your cart is empty."
+    else
+      output = ["Your cart includes the following items:"]
+      @@cart.each do |item|
+        output << item
+      end
+      output.join("\n")
+    end
+  end
 
-  # def add_item_to_cart(item_to_add)
-  #   if @@items.include?(item_to_add)
-  #     @@cart << item_to_add
-  #     resp.write "We've added #{item} to your cart."
-  #   else
-  #     resp.write "We don't have that item."
-  #   end
-  # end
+  def add_item_to_cart(item_to_add)
+    if @@items.include?(item_to_add)
+      @@cart << item_to_add
+      "We've added #{item_to_add} to your cart."
+    else
+      "We don't have that item."
+    end
+  end
 end
