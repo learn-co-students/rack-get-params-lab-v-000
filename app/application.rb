dd77b6328c978cde6fc06d050c167e5169fdd221
ingridwong0715 <ingrid830715@gmail.com>
2018-02-20 21:03:04 +0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +15,24 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      searched_item = req.params["item"]
+      if handle_search(searched_item) == "Couldn't find #{searched_item}"
+        resp.write "We don't have that item"
+      else
+        @@cart << searched_item
+        resp.write "added #{searched_item}"
+      end
+
+
     else
       resp.write "Path Not Found"
     end
@@ -28,3 +48,20 @@ class Application
     end
   end
 end
+
+
+
+#
+# describe "/cart" do
+#   it "responds with empty cart message if the cart is empty" do
+#     Application.class_variable_set(:@@cart, [])
+#     get '/cart'
+#     expect(last_response.body).to include("Your cart is empty")
+#   end
+#
+#   it "responds with a cart list if there is something in there" do
+#     Application.class_variable_set(:@@cart, ["Apples","Oranges"])
+#     get '/cart'
+#     expect(last_response.body).to include("Apples\nOranges")
+#   end
+# end
