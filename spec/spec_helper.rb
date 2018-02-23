8e2090da95bb60cbdbc2d8c7351c6dda6a27e141
jljardon <github email address>
2018-02-22 23:54:55 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require_relative '../config/environment'
 require 'rack/test'
 
 RSpec.configure do |config|
+   config.color = true
+   config.include Rack::Test::Methods
 
-  config.include Rack::Test::Methods
-
-  config.order = 'default'
+   config.order = 'default'
 end
