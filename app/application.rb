7a4f6b360067bdd8e10a110a0d64e4ee49919c9d
thesilentc <thesilentc@me.com>
2018-03-06 10:45:05 -0500

    added 'item' key

--- app/application.rb
+++ app/application.rb
@@ -11,15 +11,16 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif condition
 
-    resp.finish
-  end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
