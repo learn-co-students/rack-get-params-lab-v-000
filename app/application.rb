5b499e6179718faf7d6f98b1c6fee3c145cf5238
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-13 00:09:56 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,19 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+        if @@cart.empty?
+          resp.write "Your cart is empty"
+        else
+          resp.write "#{@@cart.join("\n")}"
+        end
+      elsif req.path.match(/add/)
+        if !@@cart.include?(req.params["item"])
+          resp.write "added Figs"
+          @@cart << req.params["item"]
+        else
+          resp.write "We don't have that item"
+        end
     else
       resp.write "Path Not Found"
     end
