7a369d8fe260018d456c0f5f34e5e63d89759ff8
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-14 18:16:21 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,9 +13,21 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 1 
+        @@cart.each do |item| 
+          resp.write "#{item}\n"
+        end
+      else 
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      resp.write handle_add(add_item)
     else
       resp.write "Path Not Found"
     end
+
 
     resp.finish
   end
@@ -28,30 +40,39 @@ class Application
     end
   end
   
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env) 
-    
-    if req.path.match(/cart/)
-      @@cart each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/add/)
-      item_search = req.params["q"]
-      resp.write handle_add(item_search)
+  def handle_add(add_item)
+    if @@items.include?(add_item)
+      @@cart << add_item 
+      return "added #{add_item}"
     else 
-      resp.write "Path not found"
-    end
-    
-    resp.finish
-  end
-      
-  def handle_add(item_search)    
-    if @@items.include(item_search)
-      @@cart << item_search 
-      return "#{item_search} was added to your cart."
-    else 
-      return "Couldn't find #{search_item}"
+      return "We don't have that item"
     end 
   end
+  
+#  def call(env)
+#    resp = Rack::Response.new
+#    req = Rack::Request.new(env) 
+#    
+#   if req.path.match(/cart/)
+#      @@cart.each do |item|
+#        resp.write "#{item}\n"
+#      end
+#    elsif req.path.match(/add/)
+#      item_search = req.params["q"]
+#      resp.write handle_add(item_search)
+##   else 
+#      resp.write "Path not found"
+#    end
+#    
+#    resp.finish
+#  end
+#      
+#  def handle_add(item_search)    
+#    if @@items.include?(item_search)
+#      @@cart << item_search 
+#      return "#{item_search} was added to your cart."
+#    else 
+#      return "Your cart is empty"
+#    end 
+#  end
 end
