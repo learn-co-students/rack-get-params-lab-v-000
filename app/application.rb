9f2f1829d96d1e60dbc26b97e2104e3c966f36c2
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-03-27 17:08:36 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,8 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,9 +12,24 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      search_item = req.params["item"]
+      resp.write handle_add(search_item)
+
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +44,20 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def handle_add(search_item)
+    if @@items.include?(search_item)
+      @@cart << search_item
+      return "added #{search_item}"
+    else
+      return "We don't have that item"
+    end
+  end
+
 end
+
+
+
+# Create a new route called /add that takes in a GET param with the key item. 
+# This should check to see if that item is in @@items and then add it to the cart if it is. 
+# Otherwise give an error
