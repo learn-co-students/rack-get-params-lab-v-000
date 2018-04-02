e4f4c138b7d8f9815266a838d90ee4c3e5beab1d
Sam. O <samrey2018@gmail.com>
2018-04-02 17:10:10 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -19,6 +19,8 @@ class Application
    elsif req.path.match(/cart/)
           if @@cart.empty?
        # or we can say @@cart.length == 0
+       binding.pry
+
            resp.write "Your cart is empty"
          else
           @@cart.each do |item_in_cart|
@@ -26,14 +28,18 @@ class Application
           end
          end
 
-      #  elsif req.path.match(/add/)
-      #       add_item = req.params["item"]
-      #     if @@items.include?(add_item)
-      #          @@cart << add_item
-      #      else
-      #           resp.write "404"
-      #    end
-      #   end
+
+
+         #this works till here. 
+
+       elsif req.path.match(/add/)
+            add_item = req.params["item"]
+          if @@items.include?(add_item)
+               @@cart << add_item
+           else
+                resp.write "404"
+         end
+        end
       #  end
 
  # >>>>>>>>>>>>
@@ -47,7 +53,7 @@ class Application
     #
     #    resp.finish
     #  end
-    #
+
     #  def handle_search(search_term)
     #    if @@items.include?(search_term)
     #      return "#{search_term} is one of our items"
@@ -58,37 +64,37 @@ class Application
 
 
 
-#     if req.path.match(/items/)
-#       # note.. you can add a if with in a if statment. (nesting if statment)
-#       if @@cart.empty?
-#         # or we can say @@cart.length == 0
-#         resp.write "Your cart is empty"
-#       else
-#         @@cart.each do |cart|
-#
-#           resp.write "#{cart}\n"
-#         end
-#       end
-#     end
-#
-#     if req.path.match(/search/)
-#       search_term = req.params["q"]
-#       if @@cart.include?(search_term)
-#         resp.write "added #{"q"}"
-#       else
-#         resp.write "Item not found"
-#       end
-#
-#     elsif req.path.match(/add/)
-#       add_item = req.params["item"]
-#       binding.pry
-#       if @@items.include?(add_item)
-#          @@cart << add_item
-#       else
-#         resp.write "404"
-#       end
-#     else
-#       resp.finish
-#     end
-#  end
- end
+ #    if req.path.match(/items/)
+ #      # note.. you can add a if with in a if statment. (nesting if statment)
+ #      if @@cart.empty?
+ #        # or we can say @@cart.length == 0
+ #        resp.write "Your cart is empty"
+ #      else
+ #        @@cart.each do |cart|
+ #
+ #          resp.write "#{cart}\n"
+ #        end
+ #      end
+ #    end
+ #
+ #    if req.path.match(/search/)
+ #      search_term = req.params["q"]
+ #      if @@cart.include?(search_term)
+ #        resp.write "added #{"q"}"
+ #      else
+ #        resp.write "Item not found"
+ #      end
+ #
+ #    elsif req.path.match(/add/)
+ #      add_item = req.params["item"]
+ #      binding.pry
+ #      if @@items.include?(add_item)
+ #         @@cart << add_item
+ #      else
+ #        resp.write "404"
+ #      end
+ #    else
+ #      resp.finish
+ #    end
+  # end
+ # end
