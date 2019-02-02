bda7b2b3ba1276b566835a32cd8805f6aea23f9c
Cheryl K <cherylkelson@icloud.com>
2019-02-02 22:47:54 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,59 @@
+require 'pry'
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-  @@cart = [] #empty arrays holds items in cart 
+  @@cart = []
+
+
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
 
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/cart/)
+
+      if @@cart.empty?
+        resp.write "Your cart is empty."
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      cart = req.params["q"]
+      resp.write handle_search(cart)
+
+
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add
+        resp.write "added #{item_to_add}"
+        @@cart << item_to_add
+      else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
-    end
+     end
 
-    resp.finish
+      resp.finish
   end
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
-      return "Couldn't find #{search_term}"
+      return "Couldn't find '#{search_term}"
     end
   end
+
 end
