7bab557cf9c2519340f0bd18335c9b47c00ed294
Cheryl K <cherylkelson@icloud.com>
2019-02-02 20:28:01 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = [] #empty arrays holds items in cart 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
