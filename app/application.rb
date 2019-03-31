1baf995364ec3168931147ddfdc83fd638a5903f
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-31 17:17:59 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,20 +9,20 @@ class Application
 
     case req.path
     when /cart/
-      if @@cart.empty?
+      if @@cart.empty? #responds with empty cart message if the cart is empty
         resp.write "Your cart is empty"
-      else
+      else #responds with a cart list if there is something in there
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
       end
     when /add/
       item_to_add = req.params["item"]
-      
-      if @@items.include?(item_to_add)
+
+      if @@items.include?(item_to_add) # Will add an item that is in the @@items list
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
-      else
+      else # Will not add an item that is not in the @@items list
         resp.write "We don't have that item"
       end
     end
