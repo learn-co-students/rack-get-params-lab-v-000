c135600dbcc2071e5caec07a0ae9f949b51239dd
Jonathan Fernandez <jfern20@gmail.com>
2019-11-28 05:39:54 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,40 +1,59 @@
- class Application 
-  
+class Application
+
   @@items = ["Apples","Carrots","Pears"]
-  @@cart = [] 
+  @@cart = []
 
-   def call(env)
-    resp = Rack::Response.new 
-     req = Rack::Request.new(env)
-   
-   
-    if req.path.match(/search/)
-    @@items.each do |item|
-     resp.write "#{item}\n"
-  
-  end 
-    item = req.params["q"]
-      if @@items.include?(item)
-        resp.write "#{item} is one of our items"
-      else
-        resp.write "Couldn't find #{item}"
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+#--------------------------------
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
       end
- else 
-  resp.write "Path Not Found "
-  resp.write "Your cart is empty" 
-   resp.write @@items << "Apples\nOranges"
-end 
- resp.finish
-end 
-
+   
+#--------------------------------
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+      
+  #-------------------------------
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
+    else
+      resp.write "Path Not Found"
+    end
+      
+  #-------------------------------
+      resp.finish
+    end
+  #-------------------------------
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
-     end
-   end
- end
+    end
+  end
+end
+
+    
 
       
       
