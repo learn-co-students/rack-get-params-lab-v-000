1003d769448bac8b94639c2063f8d034c7b4eb89
alejandra cantu <alejandra.cantu13@gmail.com>
2021-01-07 04:28:35 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,48 +1,40 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-  @@cart  = []
+   @@items = ["Apples","Carrots","Pears"]
+   @@cart  = []
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
 
-    if req.path.match(/add/)
-         search_term = req.params["?"]
-         @@items.include?(search_term) == false
-             resp.write "We don't have that item"
-           @@items << search_term
-        end
+ def call(env)
+     resp = Rack::Response.new
+     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    end
-    if req.path.match(/cart/)
-      @@cart.each do |item|
-          resp.write "#{item}\n"
-      end
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      end
+     if req.path.match(/items/)
+       @@items.each do |item|
+         resp.write "#{item}\n"
+       end
+     elsif req.path.match(/search/)
+       search_term = req.params["q"]
+       resp.write handle_search(search_term)
+     elsif req.path.match(/cart/)
+       if @@cart.empty?
+         resp.write "Your cart is empty"
+       else
+         @@cart.each do |item|
+           resp.write "#{item}\n"
+         end
+       end
+     elsif req.path.match(/add/)
+       item_to_add = req.params["item"]
+       if @@items.include? item_to_add
+         @@cart << item_to_add
+         resp.write "added #{item_to_add}"
+       else
+         resp.write "We don't have that item!"
+       end
+     else
+       resp.write "Path Not Found"
+     end
 
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-
-    else
-      resp.write "Path Not Found"
-    end
-
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-end
+     resp.finish
+   end
+ end
