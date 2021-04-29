46e3a0b5daa45c3e7f6903c3230ac2969576a34f
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-28 23:32:51 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -28,3 +29,5 @@ class Application
     end
   end
 end
+
+
