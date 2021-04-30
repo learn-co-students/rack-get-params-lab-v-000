bb3e351bb33b003b9435104a0ed554c7a0d5de52
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-30 02:19:40 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,11 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    # elsif req.path.match(/add/)
+    #   search_term = req.params["item"]
+    #   if @@items.include?(search_term)
+    #      @@cart << search_term
+    #   end
     else
       resp.write "Path Not Found"
     end
@@ -21,29 +26,29 @@ class Application
     if req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-      else
-        @@cart.map do |c|
-          resp.write "#{c}\n"
-        end
-      end
-      # elsif req.path.match(/add/)
-      #   search_term = req.params["item"]
-      #   if @@items.inlcude?(search_term)
-      #     @@cart << search_term
-      #   end
       # else
       #   @@cart.map do |c|
       #     resp.write "#{c}\n"
       #   end
       # end
+      elsif req.path.match(/add/)
+        search_term = req.params["item"]
+        if @@items.include?(search_term)
+          @@cart << search_term
+        end
+      else
+        @@cart.map do |c|
+          resp.write "#{c}\n"
+        end
+      end
     end 
     
-    if req.path.match(/add/)
-      search_term = req.params["item"]
-      if @@items.include?(search_term)
-        @@cart << search_term
-      end
-    end
+    # if req.path.match(/add/)
+    #   search_term = req.params["item"]
+    #   if @@items.include?(search_term)
+    #     @@cart << search_term
+    #   end
+    # end
 
     resp.finish
   end
